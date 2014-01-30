46e2a60f0953f8caa15443db0f33b7458dc47822
selear <selear@gmail.com>
2014-01-30 16:43:17 +0800

    使用'属性选择符'
    例如在<a>之中寻找符合条件的对象：
    1. 以mailto:开头的链接；
    2. 以.pdf结尾的链接；
    3. 以http开头，任意位置拥有henry的链；

--- Chapter02/02.js
+++ Chapter02/02.js
@@ -9,3 +9,9 @@ $(document).ready(function() {
 	$('#selected-plays > li').addClass('horizontal');	//''>'子元素组合符. 使得页面之中ul > li部分, 不再纵向排列; 而保持横向排列
 	$('#selected-plays li:not(.horizontal)').addClass('sub-level');	//':not()'否定类选择符. 让所有非顶级的li拥有背景色
 });
+
+$(document).ready(function() {
+	$('a[href^="mailto:"]').addClass('mailto');	//使得所有具有电子邮件形式的链接拥有电子邮件的小图标; 该链接的特点是起始字符串为mailto:
+	$('a[href$=".pdf"]').addClass('pdflink');	//所有拥有.pdf的链接拥有pdf图标; 链接之中以.pdf结尾
+	$('a[href^="http"][href*="henry"]').addClass('henrylink');	//属性选择符的组合使用, 选择http开头, 任意位置包含henry的所有链接; 需要特别注意的是, 组合使用, 多个方括号之间不能存在空格: 格式应当为'[][]', 而非'[] []'; 后种情况下该代码无法正确生效
+});
