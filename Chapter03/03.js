eec78b467345e57a68596c7ac28bec3fc1fd49b6
selear <selear@gmail.com>
2014-01-31 20:54:46 +0800

    第二次代码精简

--- Chapter03/03.js
+++ Chapter03/03.js
@@ -6,25 +6,28 @@
 // See README.txt for more information.
 
 $(document).ready(function() {
+
+	$('#switcher-default').addClass('selected');
+	//三个按钮之中清除按钮selected类, 当前选中按钮添加类工作提取出来作为单独的函数用于使用
+	// * 应当把通用处理程序转移到特殊处理程序上方; 再提取removeClass()到通用处理程序之中
+	$('#switcher button').on('click', function(){
+		$('body').removeClass();
+		$('#switcher button').removeClass('selected');
+		$(this).addClass('selected');
+	});
 	/*
 		处理简单事件, 启用页面上的三个按钮
 		通过点击页面上不同的按钮, 是的页面呈现不同的样式
 
 		三个按钮能够使用之后, 当前使用的样式需要在按钮上进行体现
+
+		将removeClass()提取到通用处理程序之中后, default按钮的功能selected样式的添加语句可以取消;
 	*/
-	$('#switcher-default').addClass('selected').on('click', function() { //在页面出现之后, 表示该状态已经被选中; 点击之后, 先清除所有selected的样式, 再给选中的按钮添加selected类
-		$('body').removeClass();
-	});
 	$('#switcher-narrow').on('click', function() {
-		$('body').removeClass().addClass('narrow');
+		$('body').addClass('narrow');
 	});
 	$('#switcher-large').on('click', function() {
-		$('body').removeClass().addClass('large');
+		$('body').addClass('large');
 	});
 
-	//三个按钮之中清除按钮selected类, 当前选中按钮添加类工作提取出来作为单独的函数用于使用
-	$('#switcher button').on('click', function(){
-		$('#switcher button').removeClass('selected');
-		$(this).addClass('selected');
-	});
 });
