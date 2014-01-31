94fe124e15f8df8c95781b7279974c281661055f
selear <selear@gmail.com>
2014-01-31 20:36:30 +0800

    被点击的按钮能够在页面上提醒用户当前所处的样式
    1. 显示刚刚载入页面呈现的状态
    2. 点击某个按钮之后清除所有selected类
    3. 当前点击的按钮添加selected类

--- Chapter03/03.js
+++ Chapter03/03.js
@@ -9,17 +9,25 @@ $(document).ready(function() {
 	/*
 		处理简单事件, 启用页面上的三个按钮
 		通过点击页面上不同的按钮, 是的页面呈现不同的样式
+
+		三个按钮能够使用之后, 当前使用的样式需要在按钮上进行体现
 	*/
-	$('#switcher-default').on('click', function() {
+	$('#switcher-default').addClass('selected').on('click', function() { //在页面出现之后, 表示该状态已经被选中; 点击之后, 先清除所有selected的样式, 再给选中的按钮添加selected类
 		$('body').removeClass('narrow');
 		$('body').removeClass('large');
+		$('#switcher button').removeClass('selected');
+		$(this).addClass('selected');
 	});
 	$('#switcher-narrow').on('click', function() {
 		$('body').addClass('narrow');
 		$('body').removeClass('large');
+		$('#switcher button').removeClass('selected');
+		$(this).addClass('selected');
 	});
 	$('#switcher-large').on('click', function() {
 		$('body').removeClass('narrow');
 		$('body').addClass('large');
+		$('#switcher button').removeClass('selected');
+		$(this).addClass('selected');
 	});
 });
