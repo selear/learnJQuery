2c930595e06974da7d230b3dc26f4d3c039a2769
selear <selear@gmail.com>
2014-02-04 21:37:13 +0800

    事件委托.02

--- Chapter03/03.js
+++ Chapter03/03.js
@@ -5,22 +5,30 @@
 //
 // See README.txt for more information.
 
-$(document).ready(function() {
-	//为三个按钮添加显示/隐藏的功能
-	//更好的用户提前情况是, 鼠标hover该元素时候就进行显示, 鼠标离开的时候就不再进行显示, 因此进行代码改写
-	$('#switcher').click(function(event) {
-		$('#switcher button').toggleClass('hidden');
+$(document).ready(function() { //鼠标移动到'#switcher'上面之后, 提高交互性, 表示当前选中的元素
+	$('#switcher').hover(function() {
+		$(this).addClass('hover');
+	}, function() {
+		$(this).removeClass('hover');
 	});
 });
 
-$(document).ready(function() {
-	$('#switcher').click(function(event) {	//在本function之中, this指代的是<div id='switcher'>这个元素
-		if($(event.target).is('button')) {
+$(document).ready(function() { //屏蔽点击的按钮, 不增加hidden的class
+	$('#switcher').click(function(event) {
+		if (!$(event.target).is('button')) {
+			$('#switcher button').toggleClass('hidden');
+		}
+	});
+});
+
+$(document).ready(function() { //为点击按钮添加功能, 是的点击按钮之后能够切换样式
+	$('#switcher-default').addClass('selected');
+	$('#switcher').click(function(event) {
+		if ($(event.target).is('button')) {
 			var bodyClass = event.target.id.split('-')[1];
-			$('body').removeClass.addClass(bodyClass);
+			$('body').removeClass().addClass(bodyClass);
 			$('#switcher button').removeClass('selected');
 			$(event.target).addClass('selected');
-			event.stopPropagation();
 		}
 	});
 });
